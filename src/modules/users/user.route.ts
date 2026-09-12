9f8e838ecf7261a0a9afe1d5f5e7fcb807a8ee2d
Saikat Sarkar <sarkarsaikat3467@gmail.com>
2026-09-12 22:55:02 +0600

    Optimizing The Auth Middleware With Higher Order Function

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -6,6 +6,8 @@ import config from "../../config";
 import { userController } from "./user.controller";
 import { jwtUtils } from "../../utils/jwt";
 import { UserRole } from "../../../generated/prisma/enums";
+import { catchAsync } from "../../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
 
 const router = Router();
 
@@ -15,7 +17,7 @@ declare global {
             user?: {
                 name: string,
                 email: string,
-                id:string,
+                id: string,
                 role: UserRole
             }
         }
@@ -24,41 +26,54 @@ declare global {
 
 router.post('/register', userController.RegisterUser)
 
-router.get('/me', (req: Request, res: Response, next: NextFunction) => {
-    // const cookies = req.cookies;
-    const { accessToken } = req.cookies
-    // console.log(accessToken);
 
-    // res.send("Get my profile")
+const auth = (...RequiredRoles: UserRole[]) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.accessToken 
+        // || req.headers.authorization?.startsWith("Bearer ") ? req.headers.authorization?.split(" ")[1] : req.headers.authorization;
 
-    const verifiedToken = jwtUtils.verifiedToken(accessToken, config.jwt_access_secret)
+        if (!token) {
+            throw new Error("You are not logged in, please log in to access this resource")
+        }
 
-    // console.log(varifiedToken)
+        const verifiedToken = jwtUtils.verifiedToken(token, config.jwt_access_secret)
 
-    if (typeof verifiedToken === "string") { // for verification.id type
-        throw new Error("Invalid Token");
-    }
 
-    const { email, id, name, role } = verifiedToken;
-    console.log(role);
 
-    // const requiredRoles = ["CUSTOMER","PROVIDER","ADMIN"];
-    const requireRoles = [UserRole.CUSTOMER, UserRole.PROVIDER, UserRole.ADMIN];
+        if (!verifiedToken.success) {
+            throw new Error(verifiedToken.error)
+        }
 
-    if (!requireRoles.includes(role)) {
-        return res.status(403).json({
-            success: false,
-            statusCode: httpStatus.FORBIDDEN,
-            message: "Forbidden ! You don't have permission to access this resourse"
+        const { email, id, name, role } = verifiedToken.data as JwtPayload;
 
+        if (!RequiredRoles.includes(role)) {
+            throw new Error("Forbidden ! You don't have permission to access this resourse")
+        }
+
+        const user = await prisma.user.findUnique({
+            where : {id,email,name,role}
         })
-    }
 
-    req.user = {
-        name, email,id,role
-    }
+        if(!user){
+            throw new Error("User not found")
+        }
 
-    next();
-}, userController.getMyProfile)
+        if(user?.isActive === false){
+            throw new Error("Your Account has been blocked. please contact support")
+        }
+
+        req.user ={
+            name,
+            email,
+            id,
+            role
+        }
+
+        next();
+
+    })
+}
+
+router.get('/me', auth(UserRole.ADMIN,UserRole.CUSTOMER,UserRole.PROVIDER),userController.getMyProfile)
 
 export const userRouter = router;
